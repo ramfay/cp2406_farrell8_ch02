32a13a9c90e8b55de7c20afc5c8c417a42efcbf6
ramfay <ramisa.fayez@my.jcu.edu.au>
2017-08-17 19:31:18 +1000

    debug programs

--- DebuggingExercises/DebugTwo2.java
+++ DebuggingExercises/DebugTwo2.java
@@ -7,7 +7,7 @@ public class DebugTwo2
       a = 7;
       b = 4;
       System.out.println("The sum is " + a + b);
-      System.out.println("The difference is " + a - b);
-      System.out.println("The product is " + a - b);
+      System.out.println("The difference is " + (a - b));
+      System.out.println("The product is " + a * b);
    }
 }
